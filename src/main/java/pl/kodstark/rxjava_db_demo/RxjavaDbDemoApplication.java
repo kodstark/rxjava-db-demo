07f410fd0f5a1f05e2373d3f7baa4bc62b25b24e
Kamil Demecki <kodstark@gmail.com>
2024-11-27 09:58:21 +0100

    Init repository

---
+++ src/main/java/pl/kodstark/rxjava_db_demo/RxjavaDbDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.kodstark.rxjava_db_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxjavaDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxjavaDbDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kodstark/rxjava_db_demo/RxjavaDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kodstark.rxjava_db_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RxjavaDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
